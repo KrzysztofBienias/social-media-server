a3e8fb785bb0dbf2b279cbfc042b7917f9e6d20f
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-03-18 16:00:44 +0100

    :sparkles: Created post route

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ import { fileURLToPath } from 'url';
 import process from 'process';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
+import { createPost } from './controllers/posts.js';
+import { verifyToken } from './middleware/auth.js';
 
 // config
 const __filename = fileURLToPath(import.meta.url);
@@ -42,10 +45,12 @@ const upload = multer({ storage });
 
 // files routes
 app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
 
 // routes
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 // mongoose
 const PORT = process.env.PORT || 6001;
